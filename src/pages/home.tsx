74f7429aa9ebeb954db71477bcc29ca779c21e2a
nevzl <nevzl@DESKTOP-HHF3P0M>
2024-11-18 22:54:19 +0300

    Init api server from client, settings cors

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,28 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import {
+  authControllerSignIn,
+  authControllerSignUp,
+} from "@/shared/api/generated";
+import { useEffect } from "react";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../../pages/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../../pages/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
 
 export default function HomePage() {
+  useEffect(() => {
+    authControllerSignIn({ email: "test@gail.com", password: "1234" }).then(
+      console.log,
+    );
+  }, []);
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
